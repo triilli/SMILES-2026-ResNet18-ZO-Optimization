dbd7ea49e020b1e7bf25955465f79c61a13d0957
Ann Lyakina <vasya.egorov2002@gmail.com>
2026-05-13 00:06:44 +0300

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -8,18 +8,21 @@ _CIFAR100_STD = (0.2675, 0.2565, 0.2761)
 def get_transforms(train: bool = True) -> T.Compose:
     if train:
         return T.Compose([
-            T.Resize(224),
+            
             T.RandomHorizontalFlip(),
+            T.RandomCrop(32, padding=4),
+            T.Resize(224),
             # Add more augmentations here ↓
-            T.RandomCrop(224, padding=28),
-            # T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+            
+            T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
             T.ToTensor(),
-            T.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761]),
+            T.Normalize(mean=_CIFAR100_MEAN, std=_CIFAR100_STD),
         ])
     else:
         # Fixed validation pipeline — do not modify.
         return T.Compose([
             T.Resize(224),
             T.ToTensor(),
-            T.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761]),
+            T.Normalize(mean=_CIFAR100_MEAN, std=_CIFAR100_STD),
         ])
+        
